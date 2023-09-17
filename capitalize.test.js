895bd1e97c0153dbca3ff8cf6593bad5b0f2139a
norphel <norphel317@gmail.com>
2023-09-17 17:37:12 +0530

    add more test

--- capitalize.test.js
+++ capitalize.test.js
@@ -11,3 +11,11 @@ test("capitalized version of 'a' should be 'A'", () => {
 test("capitalized version of 'HI' should be 'Hi'", () => {
   expect(capitalize("HI")).toBe("Hi");
 });
+
+test("capitalized version of 'good, morning!' should be 'Good, morning!'", () => {
+  expect(capitalize("good, morning!")).toBe("Good, morning!");
+});
+
+test("capitalized version of 'HoW aRE yOu?' should be 'How are you?", () => {
+  expect(capitalize("HoW aRE yOu?")).toBe("How are you?");
+});

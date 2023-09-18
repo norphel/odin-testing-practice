91a8ddd277f46196e21315ef818541a76173f466
norphel <norphel317@gmail.com>
2023-09-18 11:14:41 +0530

    add more test for non-string type argument to capitalize function

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,4 +1,7 @@
 function capitalize(str) {
+  if (typeof str !== "string") {
+    return "TypeError: argument to capitalize function must be of String type";
+  }
   str = str.toLowerCase();
   return str[0].toUpperCase() + str.slice(1);
 }
--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -19,3 +19,25 @@ test("capitalized version of 'good, morning!' should be 'Good, morning!'", () =>
 test("capitalized version of 'HoW aRE yOu?' should be 'How are you?", () => {
   expect(capitalize("HoW aRE yOu?")).toBe("How are you?");
 });
+
+test("capitalized version of '123abc' should be '123abc'", () => {
+  expect(capitalize("123abc")).toBe("123abc");
+});
+
+test("cannot capitalize non-string data type", () => {
+  expect(capitalize(123)).toBe(
+    "TypeError: argument to capitalize function must be of String type"
+  );
+});
+
+test("cannot capitalize non-string data type", () => {
+  expect(capitalize([1, 2, 3])).toBe(
+    "TypeError: argument to capitalize function must be of String type"
+  );
+});
+
+test("cannot capitalize non-string data type", () => {
+  expect(capitalize({ one: 1, two: 2 })).toBe(
+    "TypeError: argument to capitalize function must be of String type"
+  );
+});

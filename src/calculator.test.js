2bf212e1d47305f52c5426fa67b2d28eacc14e91
norphel <norphel317@gmail.com>
2023-09-18 12:21:40 +0530

    add tests for negative numbers

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -31,3 +31,51 @@ test("divide(12.589, 9.56) ≈ 1.32", () => {
 test("mulitply(23.609, 3.25) = 76.72925", () => {
   expect(calculator.multiply(23.609, 3.25)).toBeCloseTo(76.72925);
 });
+
+test("add(-10.56, 5.36) = -5.2", () => {
+  expect(calculator.add(-10.56, 5.36)).toBeCloseTo(-5.2);
+});
+
+test("add(10.56, -5.36) = 5.2", () => {
+  expect(calculator.add(10.56, -5.36)).toBeCloseTo(5.2);
+});
+
+test("add(-10.56, -5.36) = -15.92", () => {
+  expect(calculator.add(-10.56, -5.36)).toBeCloseTo(-15.92);
+});
+
+test("subtract(-3.98, 4.309) = -8.289", () => {
+  expect(calculator.subtract(-3.98, 4.309)).toBeCloseTo(-8.289);
+});
+
+test("subtract(3.98, -4.309) = 8.289", () => {
+  expect(calculator.subtract(3.98, -4.309)).toBeCloseTo(8.289);
+});
+
+test("subtract(-3.98, -4.309) = 0.329", () => {
+  expect(calculator.subtract(-3.98, -4.309)).toBeCloseTo(0.329);
+});
+
+test("divide(-3.98, 0.112) ≈ -35.536", () => {
+  expect(calculator.divide(-3.98, 0.112)).toBeCloseTo(-35.536);
+});
+
+test("divide(3.98, -0.112) ≈ -35.536", () => {
+  expect(calculator.divide(3.98, -0.112)).toBeCloseTo(-35.536);
+});
+
+test("divide(-3.98, -0.112) ≈ 35.536", () => {
+  expect(calculator.divide(-3.98, -0.112)).toBeCloseTo(35.536);
+});
+
+test("mulitply(-0.026, 36.28) = -0.94328", () => {
+  expect(calculator.multiply(-0.026, 36.28)).toBeCloseTo(-0.94328);
+});
+
+test("mulitply(0.026, -36.28) = -0.94328", () => {
+  expect(calculator.multiply(0.026, -36.28)).toBeCloseTo(-0.94328);
+});
+
+test("mulitply(-0.026, -36.28) = 0.94328", () => {
+  expect(calculator.multiply(-0.026, -36.28)).toBeCloseTo(0.94328);
+});

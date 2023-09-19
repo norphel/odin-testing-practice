ac74eac0517732d27806c5cb8bddd57db294c4ab
norphel <norphel317@gmail.com>
2023-09-19 19:50:41 +0530

    add few more test

--- src/caesarCipher.test.js
+++ src/caesarCipher.test.js
@@ -29,3 +29,18 @@ test("'Hello!' shifted by 0 should be 'Hello!'", () => {
 test("'How are you?' shifted by 1 should be 'Ipx bsf zpv?'", () => {
   expect(caesarCipher("How are you?", 1)).toBe("Ipx bsf zpv?");
 });
+
+test("Caesar Cipher shift by 5", () => {
+  expect(
+    caesarCipher(
+      "The Caesar cipher is one of the earliest known and simplest ciphers. It is a type of substitution cipher in which each letter in the plaintext is 'shifted' a certain number of places down the alphabet.",
+      5
+    )
+  ).toBe(
+    "Ymj Hfjxfw hnumjw nx tsj tk ymj jfwqnjxy pstbs fsi xnruqjxy hnumjwx. Ny nx f yduj tk xzgxynyzynts hnumjw ns bmnhm jfhm qjyyjw ns ymj uqfnsyjcy nx 'xmnkyji' f hjwyfns szrgjw tk uqfhjx itbs ymj fqumfgjy."
+  );
+});
+
+test("'Excuse me! Should I also check for negative shift factor?' shifted by 23 should be 'Buzrpb jb! Pelria f xipl zebzh clo kbdxqfsb pefcq cxzqlo?'", () => {
+  expect(caesarCipher("How are you?", 1)).toBe("Ipx bsf zpv?");
+});

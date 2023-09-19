5ade0d30f49e7b6b2808193b83c5be26ad73d177
norphel <norphel317@gmail.com>
2023-09-19 19:16:30 +0530

    add more test

--- src/caesarCipher.test.js
+++ src/caesarCipher.test.js
@@ -11,3 +11,13 @@ test("'abcxyz' shifted by 1 should be 'bcdyza'", () => {
 test("'ABCD' shifted by 1 should be 'BCDE'", () => {
   expect(caesarCipher("ABCD", 1)).toBe("BCDE");
 });
+
+test("'ABCDEFGHIJKLMNOPQRSTUVWXYZ' shifted by 3 should be 'DEFGHIJKLMNOPQRSTUVWXZYABC'", () => {
+  expect(caesarCipher("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 3)).toBe(
+    "DEFGHIJKLMNOPQRSTUVWXYZABC"
+  );
+});
+
+test("'TheOdinProject' shifted by 2 should be 'VjgQfkpRtqlgev'", () => {
+  expect(caesarCipher("TheOdinProject", 2)).toBe("VjgQfkpRtqlgev");
+});

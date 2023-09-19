efd353cdd0a7aacc048fdd2c8c73ae6cd718723d
norphel <norphel317@gmail.com>
2023-09-19 19:08:55 +0530

    update caesarCipher to pass required test

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -1,2 +1,16 @@
-function caesarCipher(str, shiftFactor) {}
+function getShiftedCharacter(char, shiftFactor) {
+  if (char.charCodeAt() >= 97 && char.charCodeAt() <= 122) {
+    return String.fromCharCode(
+      ((char.charCodeAt() - 97 + shiftFactor) % 26) + 97
+    );
+  }
+}
+
+function caesarCipher(str, shiftFactor) {
+  let strArr = str.split("");
+  for (let i = 0; i < strArr.length; i++) {
+    strArr[i] = getShiftedCharacter(strArr[i], shiftFactor);
+  }
+  return strArr.join("");
+}
 module.exports = caesarCipher;

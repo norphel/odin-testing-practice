a67a8c2c6b561adf5eb3e2c356f1cc58bb27e6a5
norphel <norphel317@gmail.com>
2023-09-20 22:10:42 +0530

    add tests for floating values

--- src/analyzeArray.test.js
+++ src/analyzeArray.test.js
@@ -15,3 +15,9 @@ test("average of [1, 2, 3] = 2", () => {
 test("average of [1, 8, 3, 4, 2, 6] = 4", () => {
   expect(analyzeArray([1, 8, 3, 4, 2, 6]).average).toBe(4);
 });
+
+test("average of [1.3, 8.9, 0.03, 4.28, 62.2, 16, 0.1] ≈ 13.259", () => {
+  expect(
+    analyzeArray([1.3, 8.9, 0.03, 4.28, 62.2, 16, 0.1]).average
+  ).toBeCloseTo(13.259);
+});
